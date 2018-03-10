5e5cb0e74405b4067b53e0f337743ef12b726a85
rmcdon97 <35375133+rmcdon97@users.noreply.github.com>
2018-03-10 12:49:14 -0500

    Created current hand vector script

---
+++ Assets/FindFirstCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private bool previousGrip = false;
+    private Vector3 currentPosition;
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+		if(Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip) && !previousGrip)
+        {
+            previousGrip = true;
+            currentPosition = Controller.transform.pos;
+            Controller.transform.rot;
+        }
+
+        if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
+            previousGrip = false;
+	}
+}
