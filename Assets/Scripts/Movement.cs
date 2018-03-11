619a3ad66a3a34150749c89833902de2710972e4
silkytiger <triantafyllides@gmail.com>
2018-03-10 22:37:48 -0500

    Forces

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,16 +6,24 @@ public class Movement : MonoBehaviour
 {
     public SteamVR_TrackedObject Head;
     public ControllerData LeftController, RightController;
-    public float WebPullSpeed = 5f;
+    public float WebPullForce = 5f;
     public float Gravity = -9.8f;
     public float BounceDampening = 0.5f;
 
-    public float DragCoeff;
 
     private float PlayerHeight;
     private Vector3 GravityAccel;
     private Vector3 NetVelocity;
+
     private CapsuleCollider PlayerCollider;
+
+    public float AirDensity = 0.45f;
+    public float DragCoeff = 0.45f;
+    public float CrossSectionalArea = 0.68f;
+    public float StaticFrictionCoeff = 1.0f;
+    public float KineticFrictionCoeff = 0.8f;
+    public float PlayerMass = 70f;
+    private Vector3 InstantaneousVelocity;
 
     void Awake()
     {
@@ -23,6 +31,8 @@ public class Movement : MonoBehaviour
         GravityAccel = new Vector3(0f, Gravity, 0f);
         PlayerHeight = Head.transform.position.y;
         PlayerCollider = GetComponentInChildren<CapsuleCollider>();
+
+        InstantaneousVelocity = Vector3.zero;
     }
 
     private void Update()
@@ -33,18 +43,48 @@ public class Movement : MonoBehaviour
             RightController.ShowWebLine();
     }
 
+   
+
     void FixedUpdate()
     {
+        Vector3 NetForce = PlayerMass * GravityAccel;
+   
         if (LeftController.IsConnected)
-            NetVelocity += (LeftController.ConnectionPoint - LeftController.WebSpoutPoint).normalized * WebPullSpeed;
+            NetForce += (LeftController.ConnectionPoint - LeftController.WebSpoutPoint).normalized * WebPullForce;
 
         if (RightController.IsConnected)
-            NetVelocity += (RightController.ConnectionPoint - RightController.WebSpoutPoint).normalized * WebPullSpeed;
+            NetForce += (RightController.ConnectionPoint - RightController.WebSpoutPoint).normalized * WebPullForce;
 
-        if (!LeftController.IsConnected && !RightController.IsConnected && transform.position.y > PlayerHeight+1)
-            NetVelocity += GravityAccel * Time.fixedDeltaTime;
+        if (playerStanding())
+        {
+            //calculate normal force
+            Vector3 NormalForce = -PlayerMass * GravityAccel;
 
-        Vector3 newPosition = transform.position + NetVelocity * Time.fixedDeltaTime;
+            NetForce -= NormalForce;
+
+            //calculate friction
+            Vector3 KineticFrictionForce = NetForce;
+            KineticFrictionForce.y = 0;
+            KineticFrictionForce.Normalize();
+            KineticFrictionForce *= (-1f)*(NormalForce * KineticFrictionCoeff).magnitude ;
+
+            InstantaneousVelocity.y = 0;
+        }
+        else 
+        {
+            //calculate drag
+            NetForce -=
+                AirDensity *
+                DragCoeff * 
+                CrossSectionalArea * 
+                .5f * 
+                Mathf.Pow(InstantaneousVelocity.magnitude, 2f) * 
+                InstantaneousVelocity.normalized;
+        }
+
+        InstantaneousVelocity += (NetForce / PlayerMass) * Time.fixedDeltaTime; 
+
+        Vector3 newPosition = transform.position + InstantaneousVelocity * Time.fixedDeltaTime;
 
         if (newPosition.y < PlayerHeight)
             newPosition.y = PlayerHeight;
@@ -55,7 +95,7 @@ public class Movement : MonoBehaviour
 
     public void OnCollision(Collision collision)
     {
-        NetVelocity = Vector3.Reflect(NetVelocity, collision.contacts[0].normal)*BounceDampening;
+        InstantaneousVelocity = Vector3.Reflect(InstantaneousVelocity, collision.contacts[0].normal)*BounceDampening;
     }
 
     void updateColliderPosition()
@@ -63,12 +103,28 @@ public class Movement : MonoBehaviour
         Vector3 colliderPos = Head.transform.position;
         colliderPos.y -= PlayerHeight - 1;
         PlayerCollider.transform.position = colliderPos;
-
     }
 
     private bool playerStanding()
     {
-        return transform.position.y <= PlayerHeight; 
+        return Head.transform.position.y <= PlayerHeight; 
 
     }
 }
+
+//if (LeftController.IsConnected)
+//    NetVelocity += (LeftController.ConnectionPoint - LeftController.WebSpoutPoint).normalized * WebPullSpeed;
+
+//if (RightController.IsConnected)
+//    NetVelocity += (RightController.ConnectionPoint - RightController.WebSpoutPoint).normalized * WebPullSpeed;
+
+//if (!LeftController.IsConnected && !RightController.IsConnected && transform.position.y > PlayerHeight+1)
+//    NetVelocity += GravityAccel * Time.fixedDeltaTime;
+
+//Vector3 newPosition = transform.position + NetVelocity * Time.fixedDeltaTime;
+
+//if (newPosition.y < PlayerHeight)
+//    newPosition.y = PlayerHeight;
+
+//transform.position = newPosition;
+//updateColliderPosition();
