6b41e15031090fd35709b3f32ea1a7aa897fec78
silkytiger <triantafyllides@gmail.com>
2018-03-10 20:26:29 -0500

    Better velocity control

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,12 +7,18 @@ public class Movement : MonoBehaviour
     public SteamVR_TrackedObject Head;
     public ControllerData LeftController, RightController;
     public float WebPullSpeed = 5f;
+    public float Gravity = -9.8f;
+    public float DragCoeff;
 
+    private float PlayerHeight;
+    private Vector3 GravityAccel;
     private Vector3 NetVelocity;
 
     void Awake()
     {
         NetVelocity = Vector3.zero;
+        GravityAccel = new Vector3(0f, Gravity, 0f);
+        PlayerHeight = Head.transform.position.y;
     }
 
     private void Update()
@@ -31,15 +37,20 @@ public class Movement : MonoBehaviour
         }
 
         if (LeftController.IsConnected)
-            NetVelocity += (LeftController.ConnectionPoint - LeftController.WebSpoutPoint) * WebPullSpeed;
+            NetVelocity += (LeftController.ConnectionPoint - LeftController.WebSpoutPoint).normalized * WebPullSpeed;
 
         if (RightController.IsConnected)
-            NetVelocity += (RightController.ConnectionPoint - RightController.WebSpoutPoint) * WebPullSpeed;
+            NetVelocity += (RightController.ConnectionPoint - RightController.WebSpoutPoint).normalized * WebPullSpeed;
 
-        if(!LeftController.IsConnected && !RightController.IsConnected)
+        if (!LeftController.IsConnected && !RightController.IsConnected)
+            NetVelocity += GravityAccel * Time.fixedDeltaTime;
 
+        Vector3 newPosition = transform.position + NetVelocity * Time.fixedDeltaTime;
 
-        transform.position = NetVelocity * Time.fixedDeltaTime;
+        if (newPosition.y < PlayerHeight)
+            newPosition.y = PlayerHeight;
+
+        transform.position = newPosition;
     }
 
 }
