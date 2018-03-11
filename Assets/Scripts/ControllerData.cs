a86e5b67796cb420225f33d546916110fc2c371f
silkytiger <triantafyllides@gmail.com>
2018-03-10 20:10:05 -0500

    Added movement

---
+++ Assets/Scripts/ControllerData.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerData : MonoBehaviour
+{
+    public bool IsConnected
+    {
+        get { return _IsConnected; }
+        private set { _IsConnected = value; }
+    }
+    public Vector3 ConnectionPoint
+    {
+        get
+        {
+            if (!IsConnected)
+                throw new System.Exception("It is not connected");
+            return _ConnectionPoint;
+        }
+
+        private set
+        {
+            _ConnectionPoint = value;
+        }
+    }
+    public Vector3 WebSpoutPoint
+    {
+        get { return WebSpout.transform.position; }
+    }
+
+    public float WebRange = 100f;
+    public GameObject WebSpout;
+
+    private Vector3 _ConnectionPoint;
+    private bool _IsConnected;
+
+    private int ShootableMask;
+    private LineRenderer WebLine;
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake ()
+    {
+        ShootableMask = LayerMask.GetMask("Shootable");
+        WebLine = GetComponent<LineRenderer>();
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        IsConnected = false;
+        ConnectionPoint = Vector3.zero;
+    }
+
+    void Update()
+    {
+        if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            //store the hit
+            ShootWeb();
+        }
+        else if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
+        {
+            //remove hit
+            IsConnected = false;
+            WebLine.enabled = false;
+        }
+	}
+
+    void ShootWeb()
+    {
+        Ray webRay = new Ray();
+        RaycastHit webHit;
+
+        webRay.origin = WebSpout.transform.position;
+  
+        webRay.direction = -transform.up;
+
+        IsConnected = Physics.Raycast(webRay, out webHit, WebRange, ShootableMask);
+        ConnectionPoint = webHit.point;
+    }
+
+    public void ShowWebLine()
+    {
+        if (!IsConnected)
+            throw new System.Exception("Web not connected");
+
+        WebLine.enabled = true;
+        WebLine.SetPosition(0, WebSpout.transform.position);
+        WebLine.SetPosition(1, ConnectionPoint);
+    }
+
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public SteamVR_TrackedObject Head;
+    public ControllerData LeftController, RightController;
+    public float WebPullSpeed = 5f;
+
+    private Vector3 NetVelocity;
+
+    void Awake()
+    {
+        NetVelocity = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (LeftController.IsConnected)
+            LeftController.ShowWebLine();
+        if (RightController.IsConnected)
+            RightController.ShowWebLine();
+    }
+
+    void FixedUpdate()
+    {
+        if (NetVelocity == Vector3.zero)
+        {
+            Debug.Log("AAAAA");
+        }
+
+        if (LeftController.IsConnected)
+            NetVelocity += (LeftController.ConnectionPoint - LeftController.WebSpoutPoint) * WebPullSpeed;
+
+        if (RightController.IsConnected)
+            NetVelocity += (RightController.ConnectionPoint - RightController.WebSpoutPoint) * WebPullSpeed;
+
+        if(!LeftController.IsConnected && !RightController.IsConnected)
+
+
+        transform.position = NetVelocity * Time.fixedDeltaTime;
+    }
+
+}
