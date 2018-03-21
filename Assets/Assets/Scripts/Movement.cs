95cb8a40b0d6b70a2e511844b5edb164df57afce
silkytiger <triantafyllides@gmail.com>
2018-03-21 10:55:51 -0400

    file organization, partially working NYC scene, slightly improved hands
    -all our homade assets are now in assets/assets/
    -we now have 2 scenes, MainScene and NYC
    -NYC needs its collisions fixed (maybe some issue with mesh colliders???) I couldnt debug this because I updated unity and it messed up my debugger.
    -Hands still need animations (Its harder to add to a premade model with little experience), I may need to rebuild from scratch
    -Also, I deleted some of the assets we dont need

---
+++ Assets/Assets/Scripts/Movement.cs
@@ -0,0 +1,229 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public SteamVR_TrackedObject Head;
+    public ControllerData LeftController, RightController;
+    public float WebPullForce = 5f;
+    public float Gravity = -9.8f;
+    public float BounceDampening = 0.5f;
+
+
+    private float PlayerHeight
+    {
+        get { return Head.transform.position.y - transform.position.y; }
+    }
+    private Vector3 GravityAccel;
+    private Vector3 NetVelocity;
+
+    private CapsuleCollider PlayerCollider;
+
+    public float AirDensity = 0.45f;
+    public float DragCoeff = 0.45f;
+    public float CrossSectionalArea = 0.68f;
+    public float StaticFrictionCoeff = 1.0f;
+    public float KineticFrictionCoeff = 0.8f;
+    public float PlayerMass = 70f;
+    private Vector3 InstantaneousVelocity;
+
+
+
+
+    void Awake()
+    {
+        NetVelocity = Vector3.zero;
+        GravityAccel = new Vector3(0f, Gravity, 0f);
+    //    PlayerHeight = Head.transform.position.y;
+        PlayerCollider = GetComponentInChildren<CapsuleCollider>();
+
+        InstantaneousVelocity = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (LeftController.IsConnected)
+            LeftController.ShowWebLine();
+        if (RightController.IsConnected)
+            RightController.ShowWebLine();
+    }
+
+
+    Vector3 NonClimbingPos;
+
+    void FixedUpdate()
+    {
+        if (LeftController.IsClimbing)
+        {
+
+            Vector3 positionDelta = LeftController.ClimbingStartPosition - LeftController.CurrentPosition ;
+            positionDelta.x = 0f;
+            positionDelta.z = 0f;
+            transform.position = NonClimbingPos + positionDelta;
+            InstantaneousVelocity = Vector3.zero;
+            InstantaneousVelocity.y = 7f;
+        }
+        else if (RightController.IsClimbing)
+        {
+            Vector3 positionDelta = RightController.ClimbingStartPosition - RightController.CurrentPosition;
+            positionDelta.x = 0f;
+            positionDelta.z = 0f;
+            transform.position = NonClimbingPos + positionDelta;
+            InstantaneousVelocity = Vector3.zero;
+            InstantaneousVelocity.y = 7f;
+        }
+        else
+        {
+            Vector3 NetForce = PlayerMass * GravityAccel;
+
+            if (LeftController.IsConnected && !LeftController.IsGrabbing)
+                NetForce += (LeftController.ConnectionPoint - LeftController.WebSpoutPoint).normalized * WebPullForce;
+
+            if (RightController.IsConnected && !RightController.IsGrabbing)
+                NetForce += (RightController.ConnectionPoint - RightController.WebSpoutPoint).normalized * WebPullForce;
+
+            if (playerStanding())
+            {
+                //calculate normal force
+                Vector3 NormalForce = -PlayerMass * GravityAccel;
+
+                NetForce += NormalForce;
+
+
+
+                if (!RightController.IsConnected && !LeftController.IsConnected)
+                    InstantaneousVelocity = Vector3.zero;
+
+                //calculate friction
+                //Vector3 KineticFrictionForce = NetForce;
+                //KineticFrictionForce.y = 0;
+                //KineticFrictionForce.Normalize();
+                //KineticFrictionForce *= (-1f)*(NormalForce * KineticFrictionCoeff).magnitude ;
+
+                //InstantaneousVelocity.y = 0;
+            }
+            else
+            {
+                //calculate drag
+                NetForce -=
+                    AirDensity *
+                    DragCoeff *
+                    CrossSectionalArea *
+                    .5f *
+                    Mathf.Pow(InstantaneousVelocity.magnitude, 2f) *
+                    InstantaneousVelocity.normalized;
+            }
+
+            InstantaneousVelocity += (NetForce / PlayerMass) * Time.fixedDeltaTime;
+            Vector3 newPosition = transform.position + InstantaneousVelocity * Time.fixedDeltaTime;
+
+
+            if (newPosition.y < PlayerHeight)
+                newPosition.y = PlayerHeight;
+
+            transform.position = newPosition;
+            NonClimbingPos = transform.position;
+        }
+
+        //if (LeftController.IsConnected && LeftController.IsGrabbing)
+        //{
+        //    Vector3 WebLength = (LeftController.ConnectionPoint - LeftController.WebSpoutPoint);
+        //    if (WebLength.magnitude > LeftController.ConnectionLength)
+        //        transform.position = LeftController.ConnectionPoint - WebLength.normalized * LeftController.ConnectionLength;
+        //}
+
+        //if (RightController.IsConnected && RightController.IsGrabbing)
+        //{
+        //    Vector3 WebLength = (RightController.ConnectionPoint - RightController.WebSpoutPoint);
+        //    if (WebLength.magnitude > RightController.ConnectionLength)
+        //        transform.position = RightController.ConnectionPoint - WebLength.normalized * RightController.ConnectionLength;
+        //}
+        preventDecapitation();
+        updateColliderPosition();
+    }
+
+    public void OnCollision(Collision collision)
+    {
+        Vector3 average = Vector3.zero;
+        foreach (ContactPoint cp in collision.contacts)
+            average += cp.normal;
+
+
+        average /= collision.contacts.Length;
+
+        InstantaneousVelocity = Vector3.Reflect(InstantaneousVelocity, average) * BounceDampening;
+
+        //might remove this (less elastic collisions)
+        //transform.position += (transform.position - collision.contacts[0].point).normalized * (0.5f);
+
+        //InstantaneousVelocity = Vector3.Reflect(InstantaneousVelocity, collision.contacts[0].normal)*BounceDampening;
+
+        ////might remove this (less elastic collisions)
+        //transform.position += (transform.position - collision.contacts[0].point).normalized * (0.5f);
+    }
+
+    void updateColliderPosition()
+    {
+        Vector3 colliderPos = Head.transform.position;
+        colliderPos.y -= PlayerHeight ;
+        PlayerCollider.transform.position = colliderPos;
+    }
+
+    private bool playerStanding()
+    {
+        //return Head.transform.position.y <= PlayerHeight; 
+        Ray heightRay = new Ray();
+        RaycastHit heightHit;
+
+        heightRay.origin = transform.position;
+
+        heightRay.direction = new Vector3(0f,-1f,0f);
+
+        Physics.Raycast(heightRay, out heightHit, float.MaxValue, LayerMask.GetMask("Shootable"));
+        return heightHit.distance < 0.5;
+    }
+
+    private void preventDecapitation()
+    {
+        float playerHeight = Head.transform.position.y - transform.position.y; 
+
+        Ray heightRay = new Ray();
+        RaycastHit heightHit;
+
+        heightRay.origin = Head.transform.position;
+
+        heightRay.direction = new Vector3(0f, -1f, 0f);
+
+        bool aboveShootable = Physics.Raycast(heightRay, out heightHit, float.MaxValue, LayerMask.GetMask("Shootable"));
+
+        float exposedPlayerPortion = heightRay.origin.y - heightHit.point.y;
+
+        if ( exposedPlayerPortion < playerHeight)
+        {
+
+            Vector3 pos = transform.position;
+            pos.y += (playerHeight - exposedPlayerPortion);
+            transform.position = pos;
+        }
+
+
+    }
+}
+
+//if (LeftController.IsConnected)
+//    NetVelocity += (LeftController.ConnectionPoint - LeftController.WebSpoutPoint).normalized * WebPullSpeed;
+
+//if (RightController.IsConnected)
+//    NetVelocity += (RightController.ConnectionPoint - RightController.WebSpoutPoint).normalized * WebPullSpeed;
+
+//if (!LeftController.IsConnected && !RightController.IsConnected && transform.position.y > PlayerHeight+1)
+//    NetVelocity += GravityAccel * Time.fixedDeltaTime;
+
+//Vector3 newPosition = transform.position + NetVelocity * Time.fixedDeltaTime;
+
+//if (newPosition.y < PlayerHeight)
+//    newPosition.y = PlayerHeight;
+
+//transform.position = newPosition;
+//updateColliderPosition();
---
+++ Assets/Assets/Scripts/gen_NYC.cs
@@ -0,0 +1,169 @@
+﻿using UnityEngine;
+using Wrld;
+using Wrld.Scripts.Utilities;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class gen_NYC : MonoBehaviour
+{
+    [Header("Camera/View Settings")]
+    [Tooltip("Camera used to request streamed resources")]
+    [SerializeField]
+    private Camera m_streamingCamera;
+
+    [Header("Setup")]
+    [SerializeField]
+    private string m_apiKey;
+
+    //[Tooltip("In degrees")]
+    //[Range(-90.0f, 90.0f)]
+    //[SerializeField]
+    private double m_latitudeDegrees = 40.7576;
+
+    //[Tooltip("In degrees")]
+    //[Range(-180.0f, 180.0f)]
+    //[SerializeField]
+    private double m_longitudeDegrees = -73.98616;
+
+    //[Tooltip("The distance of the camera from the interest point (meters)")]
+    //[SerializeField]
+    //[Range(300.0f, 7000000.0f)]
+    private double m_distanceToInterest = 1781.0;
+
+    //[Tooltip("Direction you are facing")]
+    //[SerializeField]
+    //[Range(0, 360.0f)]
+    private double m_headingDegrees = 0.0;
+
+    //[Header("Map Behaviour Settings")]
+    //[Tooltip("Coordinate System to be used. ECEF requires additional calculation and setup")]
+    //[SerializeField]
+    private CoordinateSystem m_coordSystem = CoordinateSystem.UnityWorld;
+
+    [Header("Theme Settings")]
+    [Tooltip("Directory within the Resources folder to look for materials during runtime. Default is provided with the package")]
+    [SerializeField]
+    private string m_materialDirectory = "WrldMaterials/";
+
+    [Tooltip("The material to override all landmarks with. Uses standard diffuse if null.")]
+    [SerializeField]
+    private Material m_overrideLandmarkMaterial = null;
+
+    //[Tooltip("Set to true to use the default mouse & keyboard/touch controls, false if controlling the camera by some other means.")]
+    //[SerializeField]
+    //private bool m_useBuiltInCameraControls = false;
+
+    [Header("Collision Settings")]
+    [Tooltip("Set to true for Terrain collisions")]
+    [SerializeField]
+    private bool m_terrainCollisions = true;
+
+    [Tooltip("Set to true for Road collision")]
+    [SerializeField]
+    private bool m_roadCollisions = true;
+
+    [Tooltip("Set to true for Building collision")]
+    [SerializeField]
+    private bool m_buildingCollisions = true;
+
+    private Api m_api;
+
+    void Awake()
+    {
+        var defaultConfig = ConfigParams.MakeDefaultConfig();
+        defaultConfig.DistanceToInterest = m_distanceToInterest;
+        defaultConfig.LatitudeDegrees = m_latitudeDegrees;
+        defaultConfig.LongitudeDegrees = m_longitudeDegrees;
+        defaultConfig.HeadingDegrees = m_headingDegrees;
+        defaultConfig.MaterialsDirectory = m_materialDirectory;
+        defaultConfig.OverrideLandmarkMaterial = m_overrideLandmarkMaterial;
+        defaultConfig.Collisions.TerrainCollision = m_terrainCollisions;
+        defaultConfig.Collisions.RoadCollision = m_roadCollisions;
+        defaultConfig.Collisions.BuildingCollision = m_buildingCollisions;
+
+        Transform rootTransform = null;
+
+        if (m_coordSystem == CoordinateSystem.UnityWorld)
+        {
+            rootTransform = transform;
+        }
+
+        try
+        {
+            Api.Create(m_apiKey, m_coordSystem, rootTransform, defaultConfig);
+        }
+        catch (InvalidApiKeyException)
+        {
+#if UNITY_EDITOR
+            EditorUtility.DisplayDialog(
+                "Error: Invalid WRLD API Key",
+                string.Format("Please enter a valid WRLD API Key (see the WrldMap script on GameObject \"{0}\" in the Inspector).",
+                    gameObject.name),
+                "OK");
+#endif
+            throw;
+        }
+
+        m_api = Api.Instance;
+
+
+    }
+
+    double prevLat = 0f;
+    double prevLong = 0f;
+
+    void Update()
+    {
+        m_api.Update();
+       // m_api.StreamResourcesForCamera(m_streamingCamera);
+        SetAllLayers(gameObject, 8);
+    }
+
+    void SetAllLayers(GameObject go, int Layer)
+    {
+        if (go == null)
+            return;
+
+        go.layer = Layer;
+
+        foreach (Transform t in go.transform)
+            if (t != null)
+                SetAllLayers(t.gameObject, Layer);
+    }
+
+
+#if UNITY_EDITOR
+    void OnDestroy()
+    {
+        OnApplicationQuit();
+    }
+#endif
+
+    void OnApplicationQuit()
+    {
+        m_api.Destroy();
+    }
+
+    void OnApplicationPause(bool isPaused)
+    {
+       // SetApplicationPaused(isPaused);
+    }
+
+    void OnApplicationFocus(bool hasFocus)
+    {
+     //   SetApplicationPaused(!hasFocus);
+    }
+
+    void SetApplicationPaused(bool isPaused)
+    {
+        if (isPaused)
+        {
+            m_api.OnApplicationPaused();
+        }
+        else
+        {
+            m_api.OnApplicationResumed();
+        }
+    }
+}
